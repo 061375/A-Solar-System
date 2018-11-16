cc3a2abbab97a776de0faf5b522065a16b907017
Jeremy Heminger <webdev@Jeremys-Mac-mini.local>
2018-11-16 10:16:57 -0800

    reorganize more

---
+++ js/canvas.js
@@ -0,0 +1,64 @@
+/** 
+ * @param {Function}
+ * */
+function buildCanvas(callback) {
+
+	W = document.getElementById('container').clientWidth;
+	H = document.getElementById('container').clientHeight;
+	// create the canvas
+	canvas = document.createElement('canvas');
+	canvas.setAttribute("id","canvas");
+	// add the canvas
+	document.getElementById('container').appendChild(canvas);
+
+	ctx = canvas.getContext('2d');
+
+	callback();
+}
+/** 
+ * clear the canvas
+ * @function clear
+ * */
+function clear() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+/** 
+ * set the size of the canvas
+ * @note currently this really only works once
+ * @function resizeHandler
+ * */
+function resizeHandler() {
+	canvas.height = HH;
+	canvas.width = WW;
+}
+/** 
+ * render the visible stuff
+ * @function render
+ * */
+function render() {
+
+	requestAnimationFrame(render);
+
+	// if true then render 
+	if(isrunnning) {
+		// clear
+		clear();
+		// save
+		ctx.save();
+		Gravity.calcAll(_objects, function(err) {
+			if(!err) {
+				// loop everything
+				for(let i=0; i<_objects.length; i++) {
+					//if(i == 0)console.log(_objects[i].get().speed);
+					_objects[i].loop();
+					_objects[i].draw();
+					//if(i == 0)console.log(_objects[i].get().speed);
+				}
+				// restore
+				ctx.restore();
+			}else{
+				isrunnning = false;
+			}
+		});
+	}
+}
---
+++ js/general.js
@@ -0,0 +1,79 @@
+/** 
+ * 
+ * */
+function toggleMenu() {
+	if(document.getElementById("menu-container").hasClass("close")) {
+		document.getElementById("menu-container").removeClass("close");
+	}else{
+		document.getElementById("menu-container").addClass("close");
+	}
+}
+/** 
+ * 
+ * @function distance
+ * @param {Number}
+ * @param {Number}
+ * @param {Number}
+ * @param {Number}
+ * @returns {Number}
+ * */
+function distance(x1,y1,x2,y2) {
+	return Math.hypot(x2-x1, y2-y1);
+}
+/** 
+ * 
+ * @function trig
+ * @param {Number}
+ * @param {Number}
+ * @param {Number}
+ * @param {Number}
+ * @param {Boolean}
+ * @returns {Mixed} 
+ * */
+function trig(x,y,r,d,array) {
+
+	if(d<0)d+=360;
+	if(d>360)d-=360;
+
+	let a = d * Math.PI / 180;
+	let xpos = r * Math.cos(a);
+	let ypos = r * Math.sin(a);
+
+    if(array) {
+    	return [
+    		xpos+x,
+    		ypos+y
+    	]
+    }else{
+    	return {
+    		x:xpos+x,
+    		y:ypos+y
+    	}
+    }
+}
+/**
+* @param {Number}
+* @param {Number}
+* @param {Number}
+* @param {Number}
+* @param {Boolean}
+*
+* @returns {Number}
+* */
+function point_direction(x1,y1,x2,y2,radians,direction)
+{
+    if (undefined === direction) direction = 0;
+    if (undefined === radians) radians = false;
+    
+    var x = x2 - x1;
+    var y = y2 - y1;
+    var angleInRadians = Math.atan2(y,x);
+    var angleInDegrees = angleInRadians * 180 / Math.PI + direction;
+    
+    if(angleInDegrees < 0)angleInDegrees+=360;
+    if(angleInDegrees > 360)angleInDegrees-=360;
+    
+    if(radians) return angleInRadians;
+  
+    return angleInDegrees;
+}
